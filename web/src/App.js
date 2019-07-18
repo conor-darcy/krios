fb072cfd5f2f90aed056392904187b4ca5c4be42
Conor <himself@conordarcy.com>
2019-07-18 16:18:51 +0100

    package json file

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Example from "./components/shared/Example/Component";
 
 function App() {
+  const abc = 1;
+  let zzz = abc * 2;
+  zzz = 4;
   return (
     <div className="App">
       <header className="App-header">
